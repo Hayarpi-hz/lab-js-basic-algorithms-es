651ef892ea827bf451e4882217e88c736ed6cdc4
Hayarpi-hz <hayarpi@amazon.com>
2025-03-29 10:55:10 +0100

    1st save

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
+console.log("¡Estoy listo!");
+
 // Iteration 1: Names and Input
-//
+// 
+let hacker1 = "María";
+
+console.log(`El nombre del conductor es ${hacker1}`);
+
+let hacker2 = "Pedro";
+
+console.log(`El nombre del navegante es ${hacker2}`);
+
+
+
 // Iteration 2: Conditionals
 
 
